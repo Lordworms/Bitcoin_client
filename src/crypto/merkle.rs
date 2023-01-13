3967a0db36da021123758f16ac2e4ef49f2071ef
Yanxin Xiang <yanxinxiang0917@outlook.com>
2023-01-13 17:23:54 -0600

    first_assignment

--- src/crypto/merkle.rs
+++ src/crypto/merkle.rs
@@ -1,29 +1,129 @@
+use ring::digest::digest;
+use ring::digest::SHA256;
 use super::hash::{Hashable, H256};
+
+#[derive(Debug, Default, Clone)]
+struct MerkleTreeNode {
+    left: Option<Box<MerkleTreeNode>>,
+    right: Option<Box<MerkleTreeNode>>,
+    hash: H256,
+}
 
 /// A Merkle tree.
 #[derive(Debug, Default)]
 pub struct MerkleTree {
+    root: MerkleTreeNode,
+    level_count: usize, // how many levels the tree has
+}
+
+/// Given the hash of the left and right nodes, compute the hash of the parent node.
+fn hash_children(left: &H256, right: &H256) -> H256 {
+    H256::from(digest(&SHA256, [left.as_ref(),right.as_ref()].concat().as_slice()))
+}
+
+/// Duplicate the last node in `nodes` to make its length even.
+fn duplicate_last_node(nodes: &mut Vec<Option<MerkleTreeNode>>) {
+    if nodes.len()&1==1
+    {
+        let last=nodes.last().unwrap();
+        nodes.push(last.clone());
+    }
 }
 
 impl MerkleTree {
     pub fn new<T>(data: &[T]) -> Self where T: Hashable, {
-        unimplemented!()
+        assert!(!data.is_empty());
+
+        // create the leaf nodes:
+        let mut curr_level: Vec<Option<MerkleTreeNode>> = Vec::new();
+        for item in data {
+            curr_level.push(Some(MerkleTreeNode { hash: item.hash(), left: None, right: None }));
+        }
+        let mut level_count = 1;
+        
+        // create the upper levels of the tree:
+        while curr_level.len() > 1 {
+            // Whenever a level of the tree has odd number of nodes, duplicate the last node to make the number even:
+            if curr_level.len() % 2 == 1 {
+                duplicate_last_node(&mut curr_level); // TODO: implement this helper function
+            }
+            assert_eq!(curr_level.len() % 2, 0); // make sure we now have even number of nodes.
+
+            let mut next_level: Vec<Option<MerkleTreeNode>> = Vec::new();
+            for i in 0..curr_level.len() / 2 {
+                let left = curr_level[i * 2].take().unwrap();
+                let right = curr_level[i * 2 + 1].take().unwrap();
+                let hash = hash_children(&left.hash, &right.hash); // TODO: implement this helper function
+                next_level.push(Some(MerkleTreeNode { hash: hash, left: Some(Box::new(left)), right: Some(Box::new(right)) }));
+            }
+            curr_level = next_level;
+            level_count += 1;
+        }
+        MerkleTree {
+            root: curr_level[0].take().unwrap(),
+            level_count: level_count,
+        }
     }
 
     pub fn root(&self) -> H256 {
-        unimplemented!()
+        return self.root.hash
     }
 
     /// Returns the Merkle Proof of data at index i
-    pub fn proof(&self, index: usize) -> Vec<H256> {
-        unimplemented!()
+    pub fn proof(&self, index: usize) -> Vec<H256> {// data at index has proof of 
+        let mut proof:Vec<H256>=Vec::new();
+        let mut now=&self.root;
+        let mut step=1;
+        let mut i=index;
+        while step<self.level_count
+        {
+            if i&1==0
+            {
+                proof.push(now.right.as_ref().unwrap().hash);
+                now=now.left.as_ref().unwrap();
+            }
+            else 
+            {
+                proof.push(now.left.as_ref().unwrap().hash);
+                now=now.right.as_ref().unwrap();
+            }
+            step+=1;
+            i>>=1;
+        }
+        proof
     }
 }
 
 /// Verify that the datum hash with a vector of proofs will produce the Merkle root. Also need the
 /// index of datum and `leaf_size`, the total number of leaves.
 pub fn verify(root: &H256, datum: &H256, proof: &[H256], index: usize, leaf_size: usize) -> bool {
-    unimplemented!()
+    let mut size=0;
+    let mut ls=leaf_size;
+    while ls>0
+    {
+        ls>>=1;
+        size+=1;
+    }
+    if size!=(proof.len()+1)
+    {
+        return false
+    }
+    let mut ans=H256::from(*datum);
+    let mut cnt=index;
+    for i in (0..proof.len()).rev()
+    {
+        println!("i is {}",i);
+        if cnt&1==0
+        {
+            ans=hash_children(&ans, &proof[i]);
+        }
+        else 
+        {
+            ans=hash_children(&proof[i], &ans);
+        }
+        cnt>>=1;
+    }
+    root.hash()==ans.hash()
 }
 
 #[cfg(test)]
@@ -63,6 +163,7 @@ mod tests {
         let input_data: Vec<H256> = gen_merkle_tree_data!();
         let merkle_tree = MerkleTree::new(&input_data);
         let proof = merkle_tree.proof(0);
+        println!("proof[0] is {:?}",proof[0]);
         assert_eq!(proof,
                    vec![hex!("965b093a75a75895a351786dd7a188515173f6928a8af8c9baa4dcff268a4f0f").into()]
         );
@@ -76,5 +177,40 @@ mod tests {
         let merkle_tree = MerkleTree::new(&input_data);
         let proof = merkle_tree.proof(0);
         assert!(verify(&merkle_tree.root(), &input_data[0].hash(), &proof, 0, input_data.len()));
+    }
+    macro_rules! gen_merkle_tree_large {
+        () => {{
+            vec![
+                (hex!("0000000000000000000000000000000000000000000000000000000000000011")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000022")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000033")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000044")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000055")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000066")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000077")).into(),
+                (hex!("0000000000000000000000000000000000000000000000000000000000000088")).into(),
+            ]
+        }};
+    }
+  
+    #[test]
+    fn proof_tree_large() {
+        let input_data: Vec<H256> = gen_merkle_tree_large!();
+        let merkle_tree = MerkleTree::new(&input_data);
+        let proof = merkle_tree.proof(5);
+        println!("{:?}",proof);
+        println!("length of proof is {}",proof.len());
+        // We accept the proof in either the top-down or bottom-up order; you should stick to either of them.
+        let expected_proof_bottom_up: Vec<H256> = vec![
+            (hex!("c8c37c89fcc6ee7f5e8237d2b7ed8c17640c154f8d7751c774719b2b82040c76")).into(),
+            (hex!("bada70a695501195fb5ad950a5a41c02c0f9c449a918937267710a0425151b77")).into(),
+            (hex!("1e28fb71415f259bd4b0b3b98d67a1240b4f3bed5923aa222c5fdbd97c8fb002")).into(),
+        ];
+        let expected_proof_top_down: Vec<H256> = vec![
+            (hex!("1e28fb71415f259bd4b0b3b98d67a1240b4f3bed5923aa222c5fdbd97c8fb002")).into(),  
+            (hex!("bada70a695501195fb5ad950a5a41c02c0f9c449a918937267710a0425151b77")).into(),
+            (hex!("c8c37c89fcc6ee7f5e8237d2b7ed8c17640c154f8d7751c774719b2b82040c76")).into(),
+        ];
+        assert!(proof == expected_proof_bottom_up || proof == expected_proof_top_down);
     }
 }
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,28 +1,70 @@
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Transaction {
+use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters,UnparsedPublicKey,ED25519};
+use ring::digest::{digest,SHA256};
+use crate::crypto::hash::{H256, Hashable};
+use rand::Rng;
+#[derive(Serialize, Deserialize, Debug, Default,Clone, Eq, PartialEq, Hash)]
+pub struct MsgInput
+{
+    pub hash:H256,
+    pub index:u8
+}
+#[derive(Serialize, Deserialize, Debug, Clone,Copy)]
+pub struct MsgOutput
+{
+    pub address:H256,
+    pub value:u32
+}
+#[derive(Serialize, Deserialize, Debug, Default,Clone)]
+pub struct Transaction 
+{
+   pub input:Vec<MsgInput>,
+   pub output:Vec<MsgOutput>,
+}
+#[derive(Clone)]
+pub struct SignedTransaction
+{
+    pub trans:Transaction,
+    pub sig:Vec<u8>,
+}
+impl Hashable for Transaction
+{
+    fn hash(&self)->H256
+    {
+        let serialized_data=bincode::serialize(&self).unwrap();
+        return digest(&SHA256,&serialized_data).into();
+    }
 }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    let bytes=bincode::serialize(&t).unwrap();
+    key.sign(&bytes)
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+    let bytes=bincode::serialize(&t).unwrap();
+    let pk=UnparsedPublicKey::new(&ED25519,public_key.as_ref().to_vec());
+    let res=pk.verify(&bytes, &signature.as_ref().to_vec());
+    res.is_ok()
 }
-
+pub fn generate_random_hash() -> H256 {
+    let mut rng = rand::thread_rng();
+    let random_bytes: Vec<u8> = (0..32).map(|_| rng.gen()).collect();
+    let mut raw_bytes = [0; 32];
+    raw_bytes.copy_from_slice(&random_bytes);
+    (&raw_bytes).into()
+}
 #[cfg(any(test, test_utilities))]
 mod tests {
     use super::*;
     use crate::crypto::key_pair;
 
     pub fn generate_random_transaction() -> Transaction {
-        Default::default()
-        //unimplemented!()
+       let input=vec![MsgInput{hash:generate_random_hash(),index:0}];
+       let output=vec![MsgOutput{address:generate_random_hash(),value:0}];
+       Transaction { input: input, output: (output) }
     }
 
     #[test]
